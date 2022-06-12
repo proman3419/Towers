18a33e25aafbead1cf5b291ff8eaec0d4a120ce8
proman3419 <proman7268@gmail.com>
2022-06-12 13:13:13 +0200

    ProblemCreator generates valid problems

--- Towers/src/main/scala/Main.scala
+++ Towers/src/main/scala/Main.scala
@@ -1,9 +1,25 @@
 import logic.Grid
+import logic.ProblemCreator
 
 object Main {
     def main(args: Array[String]) = {
-        println("Hello, world")
+/*        println("Hello, world")
         val grid = new Grid(5)
         println(grid)
+
+*/      
+        val n = 5
+        val pc = new ProblemCreator(n)
+
+        pc.init_problem()
+
+        println("RESULT AFTER INIT:")
+        for (i <- 0 until n) {
+            for (j <- 0 until n) {
+                print(pc.result(i)(j))
+            }
+            println()
+        }
+
     }
 }
--- Towers/src/main/scala/logic/ProblemCreator.scala
+++ Towers/src/main/scala/logic/ProblemCreator.scala
@@ -1,11 +1,32 @@
+package logic
+
 import scala.util.Random
 import scala.collection
+import scala.collection.mutable
 import scala.util.control.Breaks._
 
 class ProblemCreator(val n: Int){
     var result: Array[Array[Int]] = Array.ofDim[Int](n, n)
 
-    def init_problem() = {
+    def validateProblem() : Boolean = {
+        var rows = Array.fill[mutable.Set[Int]](n)(mutable.Set.empty)
+        var cols = Array.fill[mutable.Set[Int]](n)(mutable.Set.empty)
+
+        for (row <- 1 until n) {
+            for (col <- 0 until n) {
+                val currVal = result(row)(col)
+                if (rows(row).contains(currVal) ||
+                    cols(col).contains(currVal))
+                    return false
+                rows(row).add(currVal)
+                cols(col).add(currVal)
+            }
+        }
+
+        return true
+    }
+
+    def init_problem() : Unit = {
         for (i <- 0 until n) {
             var tmp = Random.shuffle(List.range(1, n+1))
             for (j <- 0 until tmp.size) {
@@ -13,18 +34,18 @@ class ProblemCreator(val n: Int){
             }
         }
 
-        println("RESULT BEFORE INIT:")
+/*        println("RESULT BEFORE INIT:")
         for (i <- 0 until n) {
             for (j <- 0 until n) {
                 print(result(i)(j))
             }
             println()
         }
-        println()
+        println()*/
 
-        var impossibles_per_col = Array.ofDim[collection.mutable.Set[Int]](n)
+        var impossibles_per_col = Array.ofDim[mutable.Set[Int]](n)
         for (i <- 0 until n) {
-            impossibles_per_col(i) = collection.mutable.Set(result(0)(i))
+            impossibles_per_col(i) = mutable.Set(result(0)(i))
         }
 
         for (row <- 1 until n){
@@ -35,10 +56,11 @@ class ProblemCreator(val n: Int){
                     breakable{
                         while (i < n){
                             changing_col = (i + col) % n
-                            if (!(impossibles_per_col(changing_col).contains(result(row)(col))) && !(impossibles_per_col(col).contains(result(row)(changing_col)))){
-                                val tmp = result(row)(col)
-                                result(row).updated(col, result(row)(changing_col))
-                                result(row).updated(changing_col, tmp)
+                            if (!(impossibles_per_col(changing_col)(result(row)(col))) && 
+                                !(impossibles_per_col(col)(result(row)(changing_col)))){
+                                var tmp = result(row)(col)
+                                result(row)(col) = result(row)(changing_col)
+                                result(row)(changing_col) = tmp
                                 break
                             }
                             i = i + 1
@@ -50,23 +72,32 @@ class ProblemCreator(val n: Int){
             for (col <- 0 until n){
                 impossibles_per_col(col).add(result(row)(col))
             }
+
+            // println("SETS IN INIT:")
+            // for (i <- 0 until n) {
+            //     for (j <- 0 until row) {
+            //         print(impossibles_per_col(i)(j))
+            //     }
+            // println(impossibles_per_col(0))
+            // }
+            // println()
+            
         }
-    }
-}
 
-object Appl {
-    def main(args: Array[String]) = {
-        val n = 5
-        val pc = new ProblemCreator(n)
-
-        pc.init_problem()
-
-        println("RESULT AFTER INIT:")
-        for (i <- 0 until n) {
-            for (j <- 0 until n) {
-                print(pc.result(i)(j))
+        val fullSet = (1 to n).toSet
+        for (col <- 0 until n) {
+            // println("fullset", fullSet)
+            // println("imp", impossibles_per_col(col))
+            val diffSet = fullSet &~ impossibles_per_col(col)
+            // println("diff", diffSet)
+            if (!diffSet.isEmpty) {
+                result(n-1)(col) = diffSet.head
             }
-            println()
         }
+
+        val isProblemValid = validateProblem()
+        println(isProblemValid)
+        if (!isProblemValid)
+            init_problem()
     }
 }
